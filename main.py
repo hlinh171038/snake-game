da891542af882083fe13cdbf7b53f579a8652b97
HL <hoanglinh171038@gmail.com>
2024-11-12 15:20:51 +0700

    3.2 point of get_rect

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ screen = pygame.display.set_mode((400, 500))
 # crete surface
 test_surface = pygame.Surface((100, 200))
 #test_surface.fill(pygame.Color('blue'))
-test_rect_from_test_surface = test_surface.get_rect(center = (200,250)) # (x,y)
+test_rect_from_test_surface = test_surface.get_rect(topright = (200,250)) # (x,y)
 # create surface cannot refix position - rect(rectangle)
 # pygame.Rect(x,y,w,h)
 test_rect = pygame.Rect(100,200,100,200)
@@ -18,6 +18,6 @@ while True:
     screen.fill((175,215,70))
     pygame.draw.rect(screen, pygame.Color('red'), test_rect)
 
-    screen.blit(test_surface, test_rect_from_test_surface)
+    screen.blit(test_surface, test_rect_from_test_surface) # pint(x,y) in the topright
     pygame.display.update() 
 
