ab530b78c05957ab4f66033c8b7a3643044fa90b
HL <hoanglinh171038@gmail.com>
2024-11-13 10:37:48 +0700

    4.2 random fruit in board

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import pygame, sys
+import pygame, sys, random
 from pygame.math import Vector2
 
 
 class FRUIT:
     def __init__(self):
         # create x,y vector (best way to control object)
-        self.x = 5
-        self.y = 4
+        self.x = random.randint(0, cell_number -1)
+        self.y = random.randint(0, cell_number -1)
         self.pos = Vector2(self.x, self.y)
     # draw fruit
     def draw_fruit(self) :
-        fruit_rect = pygame.Rect(self.pos.x,self.pos.y,cell_size,cell_size)
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_number),int(self.pos.y * cell_number),cell_size,cell_size)
         #draw rectangle
         pygame.draw.rect(screen,(126, 166,114),fruit_rect)
 
