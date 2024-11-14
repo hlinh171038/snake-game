fe90fe2f88cd74ad38a0ff1b506c569d1871fa20
HL <hoanglinh171038@gmail.com>
2024-11-14 13:36:31 +0700

    7. eat fruit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ class SNAKE:
     def __init__(self):
         self.body = [Vector2(5,10), Vector2(6,10), Vector2(7,10)]
         self.direction = Vector2(1,0) # right
+        self.newBlock = False
     def draw_snake(self):
         for block in self.body:
             x_pos = int(block.x * cell_size)
@@ -15,37 +16,63 @@ class SNAKE:
     #  1. we need some user input to move snake
     #  2. we need the timer ( we noly want to move this snake whenever this timer trigger)
     def move_snake(self):
-        # coppy list except last element
-        body_copy = self.body[:-1]
-        # insert new element + direction to the first of the list
-        body_copy.insert(0,body_copy[0]+ self.direction)
-        # return list to body again
-        self.body = body_copy[:]
-        # ? i dont want exercute it all the time, i want it exercute in certain interval in my case 150 ml/s
-        # --> create timer ( timer un pygame work in event loop)
+        if self.newBlock == True :
+            # coppy all list element
+            body_copy = self.body[:]
+            # insert new element + direction to the first of the list
+            body_copy.insert(0,body_copy[0]+ self.direction)
+            # return list to body again
+            self.body = body_copy[:]
+            # ? i dont want exercute it all the time, i want it exercute in certain interval in my case 150 ml/s
+            # --> create timer ( timer un pygame work in event loop) 
+            self.newBlock = False
+        else :
+            # coppy list except last element
+            body_copy = self.body[:-1]
+            # insert new element + direction to the first of the list
+            body_copy.insert(0,body_copy[0]+ self.direction)
+            # return list to body again
+            self.body = body_copy[:]
+            # ? i dont want exercute it all the time, i want it exercute in certain interval in my case 150 ml/s
+            # --> create timer ( timer un pygame work in event loop)
 
+    def add_block(self):
+        self.newBlock = True
 
 class FRUIT:
     def __init__(self):
-        # create x,y vector (best way to control object)
+      self.randomize()
+    # draw fruit
+    def draw_fruit(self) :
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size),int(self.pos.y * cell_size),cell_size,cell_size)
+        #draw rectangle
+        pygame.draw.rect(screen,(126, 166,114),fruit_rect)
+    def randomize(self) :
+          # create x,y vector (best way to control object)
         self.x = random.randint(0, cell_number -1)
         self.y = random.randint(0, cell_number -1)
         self.pos = Vector2(self.x, self.y)
-    # draw fruit
-    def draw_fruit(self) :
-        fruit_rect = pygame.Rect(int(self.pos.x * cell_number),int(self.pos.y * cell_number),cell_size,cell_size)
-        #draw rectangle
-        pygame.draw.rect(screen,(126, 166,114),fruit_rect)
-
 class MAIN :
     def __init__(self):
         self.snake = SNAKE()
         self.fruit = FRUIT()
     def update(self) :
         self.snake.move_snake()
+        self.check_collision()
     def draw_element(self) :
         self.fruit.draw_fruit()
         self.snake.draw_snake()
+
+    # check collision
+    def check_collision(self):
+        if self.fruit.pos == self.snake.body[0] : # check if fruit position have same block of head of our snake
+            # reposition the fruit
+            self.fruit.randomize()
+                # print('snake')
+                # print('fruit position',self.fruit.pos)
+                # print('snake head:', self.snake.body[0])
+            # add another block to the snake
+            self.snake.add_block()
 
 pygame.init() # method to start the game
 #change size (w,h) 
