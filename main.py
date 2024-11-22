e4724765fd44113fca471afdce3d5a84ace4a6cb
HL <hoanglinh171038@gmail.com>
2024-11-22 13:47:05 +0700

    9.2 tail graphic update

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ class SNAKE:
 
     def draw_snake(self):
         self.update_head_graphics()
+        self.update_tail_graphics()
         for index,block in enumerate(self.body):
             #1. We still need a rect for the póitioning
             x_pos = int(block.x * cell_size)
@@ -36,7 +37,9 @@ class SNAKE:
             if index == 0:
                 screen.blit(self.head, block_rect)
                 # 3. snake head direction is not updating
-               
+            elif index == len(self.body) -1:
+                screen.blit(self.tail, block_rect)
+
             else:
                 pygame.draw.rect(screen,(150,100,100), block_rect)
 
@@ -46,6 +49,13 @@ class SNAKE:
         elif head_relation == Vector2(-1,0) : self.head = self.head_right
         elif head_relation == Vector2(0,1) : self.head = self.head_up
         elif head_relation == Vector2(0,-1) : self.head = self.head_down
+
+    def update_tail_graphics(self):
+        tail_relation = self.body[-2] - self.body[-1]
+        if tail_relation == Vector2(1,0) : self.tail = self.tail_left
+        elif tail_relation == Vector2(-1,0) : self.tail = self.tail_right
+        elif tail_relation == Vector2(0,1) : self.tail = self.tail_up
+        elif tail_relation == Vector2(0,-1) : self.tail = self.tail_down
         # for block in self.body:
         #     x_pos = int(block.x * cell_size)
         #     y_pos = int(block.y * cell_size)
