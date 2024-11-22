6444c7976738a214378cb7f55acf17be3835e2a6
HL <hoanglinh171038@gmail.com>
2024-11-22 10:48:14 +0700

    9. head(png) of the snake

--- main.py
+++ main.py
@@ -6,12 +6,42 @@ class SNAKE:
         self.body = [Vector2(5,10), Vector2(4,10), Vector2(3,10)]
         self.direction = Vector2(1,0) # right
         self.newBlock = False
+
+        self.head_up = pygame.image.load('Graphics/head_up.png').convert_alpha()
+        self.dead_down = pygame.image.load('Graphics/head_down.png').convert_alpha()
+        self.head_right = pygame.image.load('Graphics/head_right.png').convert_alpha()
+        self.head_left = pygame.image.load('Graphics/head_left.png').convert_alpha()
+
+        self.tail_up = pygame.image.load('Graphics/tail_up.png').convert_alpha()
+        self.tail_down = pygame.image.load('Graphics/tail_down.png').convert_alpha()
+        self.tail_right = pygame.image.load('Graphics/tail_right.png').convert_alpha()
+        self.tail_left = pygame.image.load('Graphics/tail_left.png').convert_alpha()
+
+        self.body_vertical = pygame.image.load('Graphics/body_vertical.png').convert_alpha()
+        self.body_horizontal = pygame.image.load('Graphics/body_horizontal.png').convert_alpha()
+
+        self.body_tr = pygame.image.load('Graphics/body_tr.png').convert_alpha()
+        self.body_tl = pygame.image.load('Graphics/body_tl.png').convert_alpha()
+        self.body_br = pygame.image.load('Graphics/body_br.png').convert_alpha()
+        self.body_bl = pygame.image.load('Graphics/body_bl.png').convert_alpha()
+
     def draw_snake(self):
-        for block in self.body:
+
+        for index,block in enumerate(self.body):
+            #1. We still need a rect for the póitioning
             x_pos = int(block.x * cell_size)
             y_pos = int(block.y * cell_size)
-            block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
-            pygame.draw.rect(screen, (183,121, 122), block_rect)
+            block_rect = pygame.Rect(x_pos,y_pos, cell_size, cell_size)
+            #2. what direction í the face heading
+            if index == 0:
+                screen.blit(self.head_right, block_rect)
+            else:
+                pygame.draw.rect(screen,(150,100,100), block_rect)
+        # for block in self.body:
+        #     x_pos = int(block.x * cell_size)
+        #     y_pos = int(block.y * cell_size)
+        #     block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
+        #     pygame.draw.rect(screen, (183,121, 122), block_rect)
     # 1. move snake:
     #  1. we need some user input to move snake
     #  2. we need the timer ( we noly want to move this snake whenever this timer trigger)
@@ -95,7 +125,7 @@ pygame.init() # method to start the game
 cell_size = 40
 cell_number = 20
 screen = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_number))
-apple = pygame.image.load('Assets/apple.jpg').convert_alpha() # import img and convert alpha to word in pygame
+apple = pygame.image.load('Graphics/apple.png').convert_alpha() # import img and convert alpha to word in pygame
 
  # 2. timmer
 SCREEN_UPDATE = pygame.USEREVENT # customer event you csnt not trigger
