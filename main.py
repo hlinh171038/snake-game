5686b3bd221df256b5cc800ced8b7a8c757d2833
HL <hoanglinh171038@gmail.com>
2024-11-27 07:39:46 +0700

    10.3 finish draw grass

--- main.py
+++ main.py
@@ -157,12 +157,16 @@ class MAIN :
         # grass color
         grass_color = (167,209,61)
         for row in range(cell_number):
-
-            for col in range(cell_number):
-                if col % 2 == 0:
-                    grass_rect = pygame.Rect(col* cell_size,row*cell_size,cell_size,cell_size)
-                    pygame.draw.rect(screen, grass_color, grass_rect)
-        
+            if row % 2 == 0 :
+                for col in range(cell_number):
+                    if col % 2 == 0:
+                        grass_rect = pygame.Rect(col* cell_size,row*cell_size,cell_size,cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)
+            else:
+                for col in range(cell_number):
+                    if col % 2 != 0:
+                        grass_rect = pygame.Rect(col*cell_size, row*cell_size, cell_size, cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)
 
 
 pygame.init() # method to start the game
